a498f220ebdfb0adb5155c0ab72dbc6811208a0e
NicoCassio <niccassantos@gmail.com>
2025-05-04 22:53:27 -0300

    Added mise's python path

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -2,12 +2,15 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local global = vim.g
 local opt = vim.opt
 
 opt.wrap = true
 opt.colorcolumn = "80"
 
-vim.g.vimtex_view_general_viewer = "SumatraPDF.exe"
+global.python3_host_prog = "/home/nicolas/.local/share/mise/installs/python/3.13.3/bin/python"
+
+global.vimtex_view_general_viewer = "SumatraPDF.exe"
 
 -- vim.g.vimtex_view_general_viewer = "~/.local/bin/sumatrapdf.sh"
 -- vim.g.vimtex_view_general_options = "-reuse-instance @pdf"

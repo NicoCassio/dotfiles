90d60b6021b1b542eb3f80fad38e5714a2ad22e4
NicoCassio <niccassantos@gmail.com>
2025-09-20 11:10:57 -0300

    Added more files

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_tmux_default_session_name main
+    set fish_tmux_autoquit false
+    set fish_tmux_autostart true
+    set -gx ATUIN_NOBIND true
+    atuin init fish | source
+    bind -M default / _atuin_search
+end
+procs --gen-completion-out fish | source
+mise activate fish | source

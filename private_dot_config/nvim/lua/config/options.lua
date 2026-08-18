a3e1bc8b24a75c73d738824b85cf797971a01183
NicoCassio <niccassantos@gmail.com>
2026-08-18 14:28:14 -0300

    Add changes from August 2026 (OCI)

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -2,16 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local global = vim.g
-local opt = vim.opt
-
-opt.wrap = true
-opt.colorcolumn = "80"
-
-global.python3_host_prog = "/home/nicolas/.local/share/mise/installs/python/3.13.3/bin/python"
-
-global.vimtex_view_general_viewer = "SumatraPDF.exe"
-
--- vim.g.vimtex_view_general_viewer = "~/.local/bin/sumatrapdf.sh"
--- vim.g.vimtex_view_general_options = "-reuse-instance @pdf"
--- vim.g.vimtex_view_general_options_latexmk = "-reuse-instance"
+-- vim.g.python3_host_prog = "/usr/bin/python3"
+vim.opt.clipboard = "unnamedplus"
--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,13 +1,59 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    fish_vi_key_bindings default
+
     set fish_tmux_default_session_name main
     set fish_tmux_autoquit false
     set fish_tmux_autostart true
-    # set fish_tmux_autostart_once true
-    #
+
     set -gx ATUIN_NOBIND true
     atuin init fish | source
     bind -M default / _atuin_search
 end
-mise activate fish | source
-procs --gen-completion-out fish | source
+
+alias ls "eza --color=always --icons=always"
+alias lat "la --tree"
+alias python python3
+alias dcd "docker compose down"
+alias dcu "docker compose up -d"
+alias dcdu "docker compose down && docker compose up -d"
+alias dcl "docker compose logs -f"
+alias dcps "docker compose ps"
+alias hugo "docker run --rm --user $(id -u):$(id -g) -v $(pwd):/project ghcr.io/gohugoio/hugo"
+
+set -gx EDITOR nvim
+set -gx fish_greeting ""
+set -gx MANPAGER "bat -plman"
+set -gx GEM_HOME $HOME/.local/gem
+set -gx GEM_PATH $HOME/.local/gem
+set -gx PERL5LIB $HOME/perl5/lib/perl5
+set -gx PERL_LOCAL_LIB_ROOT $HOME/perl5
+
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.local/gem/bin
+fish_add_path $HOME/perl5/bin
+fish_add_path /opt/nvim-linux-arm64/bin
+
+abbr -a --position anywhere -- --help '--help | bat -plhelp'
+abbr -a --position anywhere -- -h '-h | bat -plhelp'
+
+source "$HOME/.cargo/env.fish"
+
+~/.local/bin/mise activate fish | source
+
+zoxide init fish | source
+
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    command yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+        builtin cd -- "$cwd"
+    end
+    command rm -f -- "$tmp"
+end
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
+
+# Added by Antigravity CLI installer
+set -gx PATH "/home/nicolas/.local/bin" $PATH
